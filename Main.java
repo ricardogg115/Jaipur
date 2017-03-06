de305838e3fd9dc4a08acbc9a94114f2cede8cbf
hskygod <hernandezhorus3@gmail.com>
2017-03-06 13:31:21 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+/**
+* This class is designed to combine all classes and allow them to interact with each other.
+* @author (Horus Hernandez) 
+* @version (3/1/17)
+public class Main
+{
+   
+    /**
+     * 
+     */
+    public Main()
+    {
+       
+    }
+    
+     /**
+     * 
+     */
+    public int sampleMethod(int y)
+    {
+       
+    }
+}
+
