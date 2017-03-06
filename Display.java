c4529660f42ccf6f6eac613928279d2c7a03d731
hskygod <hernandezhorus3@gmail.com>
2017-03-06 13:34:40 -0600

    Create Display.java

---
+++ Display.java
@@ -0,0 +1,35 @@
+/**
+ * The Display Class is designed to display game elements with Strings.
+ * @author (Horus Hernandez) 
+ * @version (3/1/2017)
+ */
+ 
+  /**
+     *
+     */
+    public Display()
+    {
+    
+    }
+    
+     /**
+     * 
+     */
+    public int sampleMethod(int y)
+    {
+      
+    }
+    
+    public void displayHand(){
+    }
+    
+    public void displyStack(){
+    }
+    
+    public void displayNumSeals(){
+    }
+    
+    public void displayPlayer(){
+    }
+    
+    }
