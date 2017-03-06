ac4f0231bffceb04412a4af3955d85bd8261ea3f
ricardogg115 <ricardogg115@gmail.com>
2017-03-06 13:26:56 -0600

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,31 @@
+
+
+public class Card
+{
+    private String goodType;
+    
+    /**
+    * Constructor for objects of class Card
+    */
+    
+    public Card(String str)
+    {
+      goodType = str;
+    }
+    
+    /**
+    * Returns the good type of the Card
+    * @return goodType
+    */
+    
+    public String getType()
+    {
+      return goodType;
+    }
+
+
+
+
+
+
+}
