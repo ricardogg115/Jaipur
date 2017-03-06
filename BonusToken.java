2787d73ef39661a150767e0e0905664ff010a8a8
hskygod <hernandezhorus3@gmail.com>
2017-03-06 13:42:36 -0600

    Rename bonusToken.java to BonusToken.java

---
+++ BonusToken.java
@@ -0,0 +1,22 @@
+/**
+ * The BonusToken Class is used to trade cards with tokens
+ * @author (Horus Hernandez) 
+ * @version (3/1/2017)
+ */
+ 
+  /**
+     * 
+     */
+    public BonusToken(int s)
+    {
+     
+    }
+    
+    public tokenValue(int x){
+    
+    }
+    
+    
+    }
+    
+    }
