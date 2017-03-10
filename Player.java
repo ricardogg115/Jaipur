bae197a58fec4b39678a81302a43814f7142366e
ricardogg115 <ricardogg115@gmail.com>
2017-03-10 13:32:21 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -21,4 +21,51 @@ public class Player
       this.playerStack = playerStack;
       sealOfExcellence = seal;
     } 
+    
+    /**
+    * Cards from the player's hand will exchange with those on the field.
+    */
+    public void exchange()
+    {
+    }
+    
+    public void take(int a)
+    {
+        if (playerHand.size() == 7)
+        {
+            return;
+        }
+        playerHand.add(Field.fieldCard.get(a));
+    }
+   
+    public void takeCamels()
+    {
+        ArrayList<Camel> herd = Field.camelHerd();
+        for (int a = 0; a < playerHand.size(); a++)
+        {
+            while (playerHand.size() < 7)
+            {
+                playerHand.add(herd.get(a));
+            }
+        }
+    }
+    public void winRound(Player player1, Player player2)
+    {
+    }
+    
+    public ArrayList<Card> getHand()
+    {
+        return playerHand;
+    }
+    
+    public ArrayList getStack()
+    {
+        return playerStack;
+    }
+    
+    public int getSeals()
+    {
+        return sealOfExcellence;
+    }
+    
 }
