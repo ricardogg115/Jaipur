845ad564e0f5308f70db3efb8d1742e1ba5a5eb1
Milly Flores <milly.flores2001@gmail.com>
2017-03-20 13:30:40 -0500

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,1 +1,17 @@
+import java.util.ArrayList;
 
+public class Stack
+{
+    public ArrayList<Token> tokenStack;   
+    public Stack(String str)
+    {
+        for (int Gold = 0; Gold <= 10; Gold++)
+        {
+            tokenStack.add(Gold,new Token(str,10-Gold));
+        }        
+    }
+    public boolean isEmpty()
+    {
+        return tokenStack.isEmpty();
+    }
+}
