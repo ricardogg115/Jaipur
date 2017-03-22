84fe51a74cca84715a25f8b6c11a5dcf29ce844d
ricardogg115 <ricardogg115@gmail.com>
2017-03-22 12:55:30 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -25,8 +25,15 @@ public class Player
     /**
     * Cards from the player's hand will exchange with those on the field.
     */
-    public void exchange()
+    public void exchange(int a)
     {
+        Card card = playerHand.get(a);
+        Card card2 = Field.getCard(a);
+        if (playerHand.size() < 7)
+        {
+            playerHand.add(card2);
+        }
+        Field.addToField(card);
     }
     
     public void take(int a)
@@ -50,13 +57,41 @@ public class Player
         }
     }
     
-    //public static BonusToken sellCard(Card card, int amount)
-    //{
-    //}
+    public static BonusToken sellCard(Card card, int amount)
+    {
+        ArrayList<Card> sell = new ArrayList<Card>();
+        BonusToken token = new BonusToken(amount);
+        playerStack.add(token);
+        for (int a = 0; a <= amount; a++)
+        {
+            sell.add(card);
+        }
+        for (int a = 0; a < sell.size(); a++)
+        {
+            Discard.addToPile(sell.get(a));
+        }
+        return token;
+    }
     
     
     public void winRound(Player player1, Player player2)
     {
+        int value1 = 0;
+        int value2 = 0;
+        
+        for (int a = 0; a < playerStack.size(); a++)
+        {
+            value1 = player1.playerStack.get(a).getValue();
+            value2 = player2.playerStack.get(a).getValue();
+        }
+        if (value1 > value2)
+        {
+            player1.sealOfExcellence += 1;
+        }
+        else
+        {
+            player2.sealOfExcellence += 1;
+        }
     }
     
     public ArrayList<Card> getHand()
