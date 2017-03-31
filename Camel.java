d84e1a9050dc57322e6cab4c2e96f0248b48b8ed
Diana1400 <Dianaolmos1400@gmail.com>
2017-03-31 13:45:45 -0500

    Update Camel.java

--- Camel.java
+++ Camel.java
@@ -1,15 +1,16 @@
 
 /**
- * Write a description of class Camel here.
+ * The camel class is basically holding all aspects of a Camel card. 
  * 
  * @author (Diana Olmos) 
- * @version (a version number or a date)
+ * @version (version 1)
  */
 public class Camel extends Card
 {    
     /**
      * Constructor for objects of class Camel
      */
+     //@param  str String to describe Camels. 
     public Camel(String str)
     {
         super(str);
