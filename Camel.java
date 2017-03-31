44fe790dfcabbbd5558a1c9f9e186669a7ce1cec
Diana1400 <Dianaolmos1400@gmail.com>
2017-03-31 12:47:47 -0500

    Update Camel.java

--- Camel.java
+++ Camel.java
@@ -2,7 +2,7 @@
 /**
  * Write a description of class Camel here.
  * 
- * @author (your name) 
+ * @author (Diana Olmos) 
  * @version (a version number or a date)
  */
 public class Camel extends Card
